48e4db4953b3b8ff9d4eac4a3b9ea9c34e5395bd
Swackhamer <root@localhost.localdomain>
2011-02-16 19:41:12 -0600

    dns_poller.py fixed added 2 second timeout

--- pollers/dns_poller.py
+++ pollers/dns_poller.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python
 
 import sys
-import dns.resolver
-import dns.rdatatype
-import dns.rdataclass
-import subprocess
+try:
+	import dns.resolver
+	import dns.rdatatype
+	import dns.rdataclass
+except ImportError:
+	print 'Need dnspython module located at : http://www.dnspython.org'
+	sys.exit()
+import signal
 
-def get_A_record(dns_ip, dns_name):
+def get_A_record(dns_ip, dns_name, timeout=2):
 	a = dns.resolver.get_default_resolver()
-	answers = a.query(dns_name, dns.rdatatype.A, dns.rdataclass.IN, False, None)
 
 	# Set the DNS server to query
-	a.nameservers = dns_ip	
+	ns = []
+	ns.append(dns_ip)
+	a.nameservers = ns	
 
+	def handler(signum, frame):
+		raise IOError	
+
+	signal.signal(signal.SIGALRM, handler)
+
+	# Set alarm to timeout in seconds so DNS query returns 0.0.0.0 if it fails
+	signal.alarm(timeout)
+
+	try:	
+		answers = a.query(dns_name, dns.rdatatype.A, dns.rdataclass.IN, False, None)
+	except IOError:
+		return '0.0.0.0'
+
+	signal.alarm(0)
+	
 	IPs = []
-
 	for rdata in answers:
 		IPs.append(rdata.address)
 
 	return IPs
 
 if __name__ == '__main__':
-	print get_A_record('8.8.8.8', 'youtube.com')
+	print get_A_record('192.168.1.1', 'google.com')
