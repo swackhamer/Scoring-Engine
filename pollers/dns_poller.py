3096531852d1cc3c385392548db390809c8eb335
Swackhamer <root@localhost.localdomain>
2011-02-16 18:32:00 -0600

    updated dns_poller.py

--- pollers/dns_poller.py
+++ pollers/dns_poller.py
@@ -2,7 +2,23 @@
 
 import sys
 import dns.resolver
+import dns.rdatatype
+import dns.rdataclass
+import subprocess
+
+def get_A_record(dns_ip, dns_name):
+	a = dns.resolver.get_default_resolver()
+	answers = a.query(dns_name, dns.rdatatype.A, dns.rdataclass.IN, False, None)
+
+	# Set the DNS server to query
+	a.nameservers = dns_ip	
+
+	IPs = []
+
+	for rdata in answers:
+		IPs.append(rdata.address)
+
+	return IPs
 
 if __name__ == '__main__':
-    d = dns.resolver('dns/resolver.conf')
-    print d
+	print get_A_record('8.8.8.8', 'youtube.com')
