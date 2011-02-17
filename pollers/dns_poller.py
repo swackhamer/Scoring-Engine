b26f1fbb596d1c0bcd809b491c2ca4312e3b8718
Swackhamer <root@localhost.localdomain>
2011-02-16 19:55:57 -0600

    added MX lookup

--- pollers/dns_poller.py
+++ pollers/dns_poller.py
@@ -10,6 +10,35 @@ except ImportError:
 	sys.exit()
 import signal
 
+def get_MX_record(dns_ip, dns_name, timeout=2):
+    a = dns.resolver.get_default_resolver()
+
+    # Set the DNS server to query
+    ns = []
+    ns.append(dns_ip)
+    a.nameservers = ns
+
+    def handler(signum, frame):
+        raise IOError
+
+    signal.signal(signal.SIGALRM, handler)
+
+    # Set alarm to timeout in seconds so DNS query returns 0.0.0.0 if it fails
+    signal.alarm(timeout)
+
+    try:
+        answers = a.query(dns_name, dns.rdatatype.MX, dns.rdataclass.IN, False, None)
+    except IOError:
+        return '0.0.0.0'
+
+    signal.alarm(0)
+
+    IPs = []
+    for rdata in answers:
+        IPs.append(rdata.exchange)
+
+    return IPs
+
 def get_A_record(dns_ip, dns_name, timeout=2):
 	a = dns.resolver.get_default_resolver()
 
@@ -41,3 +70,4 @@ def get_A_record(dns_ip, dns_name, timeout=2):
 
 if __name__ == '__main__':
 	print get_A_record('192.168.1.1', 'google.com')
+	print get_MX_record('192.168.1.1', 'gmail.com')
